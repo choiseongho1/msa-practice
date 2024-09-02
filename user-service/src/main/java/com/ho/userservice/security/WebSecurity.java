14398f44da37672dbb5f483eb6e5c3563a871876
choiseongho <choiseongho@unfine.co.kr>
2024-09-02 15:51:09 +0900

    20240902

--- user-service/src/main/java/com/ho/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/ho/userservice/security/WebSecurity.java
@@ -31,8 +31,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
+
                 .hasIpAddress("192.168.219.113")
                 .and()
                 .addFilter(getAuthenticationFilter());
